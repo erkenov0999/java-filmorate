eff965be1e7ef732cf933ea641f8319066218ce1
erkenov0999 <erkenov0999@gmail.com>
2025-09-22 20:33:46 +0300

    Новые изменения

--- src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
 public class MPA {
+    @NotNull(message = "id не может быть пустым")
     private int id;
+
+    @NotNull(message = "Наименование ограничения не может быть пустым")
     private String mpaName;
 }
