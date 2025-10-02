b2818c38d0a2b66fe5bdb842b557fee96a7271b4
erkenov0999 <erkenov0999@gmail.com>
2025-10-02 23:08:45 +0300

    Добавил схему (schema.sql)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+    id          INT             GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email       VARCHAR(50)     NOT NULL UNIQUE,
+    login       VARCHAR(50)     NOT NULL UNIQUE,
+    name        VARCHAR(50),
+    birthday    DATE
+);
+
+
+CREATE TABLE films (
+    id              INT                 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name            VARCHAR(100)        NOT NULL,
+    description     VARCHAR(200)        NOT NULL,
+    releaseDate     DATE                NOT NULL,
+    duration        INT                 NOT NULL,
+);
+
+
+CREATE TABLE genres (
+    id      INT                 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name    VARCHAR(50)         NOT NULL
+);
+
+
+CREATE TABLE mpa (
+    id      INT                 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name    VARCHAR(50)         NOT NULL
+);
