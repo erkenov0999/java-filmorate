f31a59be50f5293c1c581041ed47076e6f8f0cc1
erkenov0999 <erkenov0999@gmail.com>
2025-10-02 23:32:06 +0300

    Изменил схему (schema.sql) "IF NOT EXISTS", нужно разобраться

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id          INT             GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email       VARCHAR(50)     NOT NULL UNIQUE,
     login       VARCHAR(50)     NOT NULL UNIQUE,
@@ -7,22 +7,23 @@ CREATE TABLE users (
 );
 
 
-CREATE TABLE films (
+CREATE TABLE IF NOT EXISTS films (
     id              INT                 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name            VARCHAR(100)        NOT NULL,
     description     VARCHAR(200)        NOT NULL,
-    release_date     DATE                NOT NULL,
+    release_date    DATE                NOT NULL,
     duration        INT                 NOT NULL,
+    mpa_id          INT                 REFERENCES mpa(id)
 );
 
 
-CREATE TABLE genres (
+CREATE TABLE IF NOT EXISTS genres (
     id      INT                 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name    VARCHAR(50)         NOT NULL
 );
 
 
-CREATE TABLE mpa (
+CREATE TABLE IF NOT EXISTS mpa (
     id      INT                 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name    VARCHAR(50)         NOT NULL
 );
