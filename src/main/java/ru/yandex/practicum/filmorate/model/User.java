0aa9f44df21ca3830796d9d1662b049d7f249254
erkenov0999 <erkenov0999@gmail.com>
2025-10-08 16:25:58 +0300

    Обноление

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,15 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
-@Setter
+@Data
 public class User {
     private Set<Long> friends = new HashSet<>();
 
