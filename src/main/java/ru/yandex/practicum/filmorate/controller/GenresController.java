5879cb9e5631f2923d2788df99f5c2a1b9a01679
erkenov0999 <erkenov0999@gmail.com>
2025-10-25 17:38:50 +0300

    Вроде готово

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenresController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenresController.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.service.GenresService;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RestController
