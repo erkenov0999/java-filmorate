6ece418f59334a5dfe025f783e13fd69a01ecff1
erkenov0999 <erkenov0999@gmail.com>
2025-10-26 13:01:43 +0300

    Вроде готово

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -46,8 +46,7 @@ public class FilmController {
     public Film getFilmById(@PathVariable long id) {
         log.info("Получение фильма с ID: {}", id);
         return filmService.getFilmById(id).
-                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        "Фильм с ID " + id + " не найден"));
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Фильм с ID " + id + " не найден"));
     }
 
     @PutMapping("/{id}/like/{userId}")
